4688ca5417d5ad30b8b7798eb9062b81ce9cea05
Marc Dutoo <marc.dutoo@openwide.fr>
2014-11-13 16:19:01 +0100

    improved & upgraded token.js to latest Kernel : * by default request.bin is not used anymore (rather does a browser-like login    and parses redirect Location response header), * separated app from dc id & secret, * upgraded Kernel (redirect_uri checks) * added test & tokeninfo requests, better debug (checks authorize case) & doc

--- token.js
+++ token.js
@@ -1,6 +1,13 @@
+// Use : ex. node token.js ./config-prod.json (mind the ./)
+
 var request = require('request');
 var request = request.defaults({jar: true});
 var conf = require('./config');
+if (process.argv.length > 2) {
+  conf = require(process.argv[2]);
+  console.log('loading conf ' + process.argv[2]);
+}
+console.log('conf : ', conf);
 
 var regCode = /code:<\/strong> (\w+)<\/p>/g;
 
@@ -13,10 +20,10 @@ var getRequestBin = {
 //Get a bin first
 request(getRequestBin, function(err, res, body) {
   var binId = JSON.parse(body).name;
-	console.log(binId);
+	console.log("requestBinId : " + binId);
 	login(binId, function() {
-    getCodeFromKernel(binId, function(code) {
-      getCodeFromBin(binId, function(code) {
+    getCodeFromKernel(binId, function(binId, code) {
+      getCodeFromBin(binId, code, function(code) {
         getToken(binId, code, function(token) {           
           console.log('\n**********TOKEN**********');
           console.log(token);
@@ -35,7 +42,7 @@ var login = function(requestBinId, callback) {
   //If you want to use authUrl directly, you'd better comment 
   //getCodeFromKernel(binId, function(code) { }); above.
   var authUrl = conf.kernelBaseUrl + '/a/auth?response_type=code'
-    + '&client_id=' + conf.client_id
+    + '&client_id=' + conf.app_client_id
     + '&scope=openid%20datacore%20profile%20email'
     + '&redirect_uri=http://requestb.in/' + requestBinId;
 
@@ -43,10 +50,13 @@ var login = function(requestBinId, callback) {
     rejectUnauthorized: false,
     url: conf.kernelBaseUrl + '/a/login',
     method: 'POST',
-    followAllRedirects: true,
+    followAllRedirects: true, // let redirect so that cookie will be stored (from Set-Cookie response header)
     headers: {
-      referer: conf.kernelBaseUrl + '/a/login'
+      referer: conf.kernelBaseUrl + '/a/login',
+      'Accept-Encoding': 'gzip, deflate' // since Sept. 2014 else 500 error
     },
+    jar: true, // store cookie in cookie.txt
+    ///followRedirect: false, // not to uselessly go to 'continue' URL
     form: {
       u: conf.login,
       pwd: conf.password,
@@ -55,11 +65,14 @@ var login = function(requestBinId, callback) {
       //'continue': authUrl
     }
   };
+  console.log("login options : ", options);
   request(options, function(err, res, body) {
     if(err) {
-      console.log(err);
+      console.log("Remote error in login() : " + err);
+      return;
     }
-    //console.log(body);
+    console.log("login res headers : ", res.headers);
+    ///console.log("login body : ", body);
     callback(requestBinId);
   });
 };
@@ -70,27 +83,59 @@ var getCodeFromKernel = function(requestBinId, callback) {
     rejectUnauthorized: false,
     url: conf.kernelBaseUrl + '/a/auth',
     method: 'POST',
-    followAllRedirects: true,
+    ///followAllRedirects: true, // don't redirect to be able to extract code from Location header
+    headers: {
+      referer: conf.kernelBaseUrl + '/a/login',
+      'Accept-Encoding': 'gzip, deflate' // since Sept. 2014 else 500 error
+    },
     form: {
       response_type: 'code',
-      client_id: conf.client_id,
-      client_secret: conf.client_secret,
+      client_id: conf.app_client_id,
+      client_secret: conf.app_client_secret,
       scope: 'openid datacore profile email',
-      redirect_uri: 'http://requestb.in/' + requestBinId
+      //redirect_uri: 'http://requestb.in/' + requestBinId // NO since Sept. 2014 has to be an approved app ex. portal
+      redirect_uri: conf.redirect_uri
+      /*state: 'a',
+      nonce: 'b'*/ // not required
     }
   };
+  console.log("getCodeFromKernel params : ", options.form);
   
   request(options, function(err, res, body) {
+    // TODO approve if asked to
     if(err) {
-      console.log(err);
+      console.log("Remote error getCodeFromKernel : " + err);
+      return;
     }
-    //console.log(body);
-    callback(requestBinId);
+    console.log("getCodeFromKernel res headers: ", res.headers);
+    var location = res.headers.location;
+    if (!location) {
+      if (body.indexOf("<html>") != -1 && body.indexOf("Authorize") != -1) {
+        console.log("Must authorize first, browse to URL...", body);
+        return;
+      }
+      console.log("Bad response getCodeFromKernel (no location) : ", body, res.headers);
+      return;
+    }
+    var beforeLocationCode = "code=";
+    var beforeLocationCodeIndex = location.indexOf(beforeLocationCode);
+    if (location.indexOf(beforeLocationCode) === -1) {
+      console.log("Bad response getCodeFromKernel, location without code : ", location);
+      return;
+    }
+    var code = location.substring(beforeLocationCodeIndex + beforeLocationCode.length);
+    ///console.log("getCodeFromKernel body: ", body);
+    console.log("getCodeFromKernel code: ", code);
+    callback(requestBinId, code);
   });
 };
 
 
-var getCodeFromBin = function(requestBinId, callback) {
+var getCodeFromBin = function(requestBinId, code, callback) {
+  if (code) {
+    return callback(code);
+  }
+
   var options = {
     url: 'http://requestb.in/' + requestBinId + '?inspect',
     method: 'GET',
@@ -99,12 +144,13 @@ var getCodeFromBin = function(requestBinId, callback) {
 
   request(options, function(err, res, body) {
     if(err) {
-      console.log(err);
+      console.log("Remote error in getCodeFromBin() : " + err);
+      return;
     }
-    //console.log(body);
+    //console.log("getCodeFromBin body : " + body);
     var code = regCode.exec(body);
     if(code) {
-      //console.log(code[1]);
+      console.log("code : " + code[1]);
       callback(code[1]);
     } else {
       callback(null);
@@ -114,27 +160,29 @@ var getCodeFromBin = function(requestBinId, callback) {
 
 
 var getToken = function(requestBinId, code, callback) {
-  var auth = 'Basic ' + new Buffer(conf.client_id + ':' + conf.client_secret).toString("base64");
+  var appBasicAuth = 'Basic ' + new Buffer(conf.app_client_id + ':' + conf.app_client_secret).toString("base64");
   var options = {
     rejectUnauthorized: false,
     url: conf.kernelBaseUrl + '/a/token',
     method: 'POST',
     headers: {
-      'Authorization': auth
+      'Authorization': appBasicAuth
     },
     form: {
       grant_type: 'authorization_code',
-      redirect_uri: 'http://requestb.in/' + requestBinId,
+      //redirect_uri: 'http://requestb.in/' + requestBinId // NO since Sept. 2014 has to be an approved app ex. portal
+      redirect_uri: conf.redirect_uri,
       code: code
     }
   };
 
+  console.log("getToken req : ", options);
   request(options, function(err, res, body) {
     if(err) {
-      console.log(err);
+      console.log("Remote error in getToken() : " + err);
+      return;
     }
-    console.log(body);
-    //console.log(JSON.parse(body).id_token);
+    console.log("getToken body ", body);
     callback(JSON.parse(body).access_token);
   });
 };
@@ -152,10 +200,55 @@ var testToken = function(token, callback) {
   
   request(options, function(err, res, body) {
     if(err) {
-      console.log(err);
+      console.log("Remote error in testToken() : " + err);
+      return;
     }
     console.log('Should display information about the user:');
     console.log(body);
+  
+  request({
+    rejectUnauthorized: false,
+    url: conf.datacoreTestUrl,
+    method: 'GET',
+    headers: {
+      'Authorization': 'Bearer ' + token,
+      'Accept': 'application/json'
+    }
+  }, function(err, res, body) {
+    if(err) {
+      console.log("Remote error in testToken() : " + err);
+      return;
+    }
+    console.log('Should display information about Datacore Resource ' + conf.datacoreTestUrl + ':');
+    console.log(body);
+  
+  var dcBasicAuth = 'Basic ' + new Buffer(conf.dc_client_id + ':' + conf.dc_client_secret).toString("base64");
+  request({
+    rejectUnauthorized: false,
+    url: conf.kernelBaseUrl + '/a/tokeninfo',
+    method: 'POST',
+    headers: {
+      'Authorization': dcBasicAuth,
+      'Accept': 'application/json'
+    },
+    form: {
+      token_type_hint: 'access_token',
+      token: token
+    }
+  }, function(err, res, body) {
+    if(err) {
+      console.log("Remote error in testToken() : " + err);
+      return;
+    }
+    console.log('Should display information about token from ' + conf.dc_client_id + ':');
+    //console.log(res);
+    console.log(body);
+    console.log(" and sub_groups: " + JSON.parse(body)["sub_groups"]);
+
+  });
+
+  });
+
     callback();
   });
 };
